3e3cfd5599b6eb6674064a7cc91fde3a20df81c8
Sebastian Alvarez <sebastian.alvarezmu@gmail.com>
2026-02-19 03:30:03 -0300

    se crea place holders para el web component

--- apps/weather-crypto-widget/src/my-widget.ts
+++ apps/weather-crypto-widget/src/my-widget.ts
@@ -4,6 +4,14 @@ import { customElement } from 'lit/decorators.js';
 @customElement('my-widget')
 export class MyWidget extends LitElement {
   render() {
-    return html`<h1>Hola Mundo</h1>`;
+    return html`<div>
+      <h1>Santiago, CL</h1>
+      <span> 18 Feb, 2026 - 13:40</span>
+      <div>☀️ 27°C Despejado</div>
+      <hr />
+      <div>Humedad 42% Viento 12 km/h Máx/Mín 29° / 18°</div>
+      <hr />
+      <div>Bitcoin BTC/USD $104,283 +2.4% ↑</div>
+    </div>`;
   }
 }
